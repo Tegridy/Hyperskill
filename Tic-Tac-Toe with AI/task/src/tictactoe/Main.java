89aaae0d9aecface3ae11203477ff03ea008a846
Yankes <jarjak4@gmail.com>
2019-05-05 20:07:11 +0200

    Stage 7

--- Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -6,31 +6,25 @@ public class Main {
     private static Scanner sc = new Scanner(System.in);
     private static char[][] board = new char[3][3];
     private static Random randNum = new Random();
+    private static int numOfX = 0;
+    private static int numOfO = 0;
 
     public static void main(String[] args) {
 
         inputCells();
         printBoard();
-
-           // inputPos();
-            easyMove();
-
+        //try {
+            do {
+                inputPos();
+                easyMove();
+            } while (!checkWinner());
+//        } catch (ArrayIndexOutOfBoundsException e){
+//            System.out.println("Draw");
+//        }
     }
 
-    /*
-    if (isImpossible(board, numOfO, numOfX))
-        System.out.println("Impossible");
-    else if(checkScore(board, 'X') )
-        System.out.println("X wins");
-    else if (checkScore(board, 'O'))
-        System.out.println("O wins");
-    else if (numOfO == 4 && numOfX == 5 || numOfO == 5 && numOfX == 4)
-        System.out.println("Draw");
-    else
-        System.out.println("Game not finished");
-    }
 
-    private static boolean checkScore (char[][] board, char val){
+    private static boolean checkScore (char val) {
         return (board[0][0] == val && board[0][1] == val && board[0][2] == val ||
                 board[1][0] == val && board[1][1] == val && board[1][2] == val ||
                 board[2][0] == val && board[2][1] == val && board[2][2] == val ||
@@ -41,35 +35,56 @@ public class Main {
                 board[0][0] == val && board[1][1] == val && board[2][2] == val);
     }
 
-    private static boolean isImpossible (char[][] board, int numOfO, int numOfX){
+    private static boolean isImpossible (){
         return (numOfO - numOfX >=2 || numOfX - numOfO >= 2 ||
                 board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X' &&
                 board[1][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O' ||
                 board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X' &&
                 board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O');
     }
-    */
-    private static void inputCells(){
-        System.out.print("Enter cells: ");
-        String cells = sc.nextLine().replaceAll("\"", "");
-        if(cells.length()<1) {
-            inputCells();
+
+    private static boolean checkWinner(){
+//        if (isImpossible()) {
+//            System.out.println("Impossible");
+//            return true;
+//        }
+        if(checkScore('X')) {
+            System.out.println("X wins");
+            return true;
         }
-        else {
-            int numOfX = 0;
-            int numOfO = 0;
+        else if (checkScore('O')) {
+            System.out.println("O wins");
+            return true;
+        }
+        else if (numOfO == 4 && numOfX == 5 || numOfO == 5 && numOfX == 4) {
+            System.out.println("Draw");
+            return true;
+        }
+//        else
+//            System.out.println("Game not finished");
+        return false;
+    }
+
+    private static void inputCells(){
+//        System.out.print("Enter cells: ");
+//        String cells = sc.nextLine().replaceAll("\"", "");
+//        if(cells.length()<1) {
+//            inputCells();
+//        }
+//        else {
 
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    char temp = cells.charAt(i * 3 + j);
-                    if (temp == 'O') {
-                        board[i][j] = temp;
-                        numOfO++;
-                    } else if (temp == 'X') {
-                        board[i][j] = temp;
-                        numOfX++;
-                    } else if (temp == ' ') board[i][j] = temp;
-                }
+                    board[i][j] = ' ';
+                    //char temp = cells.charAt(i * 3 + j);
+                    //if (temp == 'O') {
+                       // board[i][j] = temp;
+                       // numOfO++;
+                    //} else if (temp == 'X') {
+                    //    board[i][j] = temp;
+                    //    numOfX++;
+                    //} else if (temp == ' ') board[i][j] = temp;
+                //}
             }
         }
     }
@@ -100,7 +115,6 @@ public class Main {
 
     private static void inputPos() {
            do {
-               //System.out.print("Enter the coordinates: ");
                try {
                    System.out.print("Enter the coordinates: ");
                    int posY = sc.nextInt() - 1;
@@ -113,6 +127,7 @@ public class Main {
                    } else {
                        board[posX][posY] = 'X';
                        printBoard();
+                       numOfX++;
                        break;
                    }
                } catch (InputMismatchException e) {
@@ -122,15 +137,16 @@ public class Main {
            } while (true);
        }
 
-
        private static void easyMove(){
            int y = randNum.nextInt(3) + 1;
            int x = randNum.nextInt(3) + 1;
            x = setRow(x);
-           System.out.println("Making move level \"easy\"");
-           board[x][y] = 'O';
-
-           printBoard();
+           if(board[x][y-1] == ' ') {
+               System.out.println("Making move level \"easy\"");
+               board[x][y - 1] = 'O';
+               printBoard();
+               numOfO++;
+           }
     }
 }
 
