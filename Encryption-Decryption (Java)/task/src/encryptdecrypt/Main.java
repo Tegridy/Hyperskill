727deaedf6bf5b9488bb129bd38a55e73a628595
yankes <jankes1122@gmail.com>
2019-07-11 20:36:53 +0200

    Stage 3

--- Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
@@ -6,33 +6,46 @@ import java.util.Scanner;
 public class Main {
 
     private static Scanner sc = new Scanner(System.in);
+    //private static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
 
     public static void main(String[] args) {
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        int alphaLen = alphabet.length() - 1;
+        String command = sc.nextLine();
 
+        switch (command){
+            case "enc":
+                encrypt();
+                break;
+            case "dec":
+                decrypt();
+                break;
+        }
+    }
+
+
+    private static void encrypt(){
         String str = sc.nextLine();
         int key = Integer.parseInt(sc.nextLine());
-        int shift;
+
+        char[] strArr = str.toCharArray();
+
+        for(int i = 0; i < strArr.length; ++i){
+            strArr[i] = (char)(strArr[i] + key);
+        }
+
+        System.out.println(new String(strArr));
+    }
+
+    private static void decrypt(){
+        String str = sc.nextLine();
+        int key = Integer.parseInt(sc.nextLine());
+
+        key *= -1;
 
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < str.length() ; i++) {
-
-                int index = alphabet.indexOf(str.charAt(i));
-
-                if (str.charAt(i) == ' ' || (str.charAt(i) + "").matches("[\\!\\?\\#\\@\\&\\*\\$]")){
-                    result.append(str.charAt(i));
-                    continue;
-                }
-
-                shift = index + key;
-
-                while (shift > alphaLen){
-                    shift = shift - alphaLen - 1;
-                }
-
-                result.append(alphabet.charAt(shift));
+        for (char c : str.toCharArray()){
+            result.append((char)(c+key));
         }
 
         System.out.println(result.toString());
