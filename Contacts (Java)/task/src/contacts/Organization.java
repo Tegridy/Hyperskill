344357169c33edeb76b0279401cb4c11c6ef58dc
Tegridy <jarjak4@gmail.com>
2019-08-01 14:20:33 +0200

    Stage 4 final

--- Contacts (Java)/task/src/contacts/Organization.java
+++ Contacts (Java)/task/src/contacts/Organization.java
@@ -1,51 +1,52 @@
 package contacts;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
-public class Organization extends ContactBook {
+public class Organization extends Contact {
     private String name;
     private String address;
-    private String phoneNumber;
     private LocalDateTime timeCreated;
-    LocalDateTime timeEdited = LocalDateTime.of(2019, 1, 1, 0, 0);
+    LocalDateTime timeEdited = LocalDateTime.of(2019, 1, 1, 0,0);
 
     public Organization(String name, String address, String phoneNumber) {
         this.name = name;
         this.address = address;
         this.phoneNumber = phoneNumber;
-        timeCreated = LocalDateTime.now();
+        this.timeCreated = LocalDateTime.now();
+    }
+
+    @Override
+    String showName() {
+        return name;
+    }
+
+    @Override
+    void edit() {
+        System.out.print("Select a field (name, address, number): ");
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        switch (command) {
+            case "name":
+                System.out.print("Enter name: ");
+                this.name = sc.nextLine();
+                break;
+            case "address":
+                System.out.print("Enter address: ");
+                this.address = sc.nextLine();
+                break;
+            case "number":
+                System.out.print("Enter number: ");
+                this.phoneNumber = Main.isNumberValid(sc.nextLine());
+                break;
+        }
+        timeEdited = LocalDateTime.now();
+        System.out.println("Saved");
     }
 
     @Override
     public String toString() {
         return "Organization name: " + name + "\nAddress: " + address + "\nNumber: " + phoneNumber
                 + "\nTime created: " + timeCreated + "\nTime last edit: " + timeEdited;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    @Override
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 }
--- Contacts (Java)/task/src/contacts/Person.java
+++ Contacts (Java)/task/src/contacts/Person.java
@@ -1,72 +1,65 @@
 package contacts;
 
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
-public class Person extends ContactBook {
+public class Person extends Contact {
 
     private String firstName;
     private String lastName;
     private String birthDate;
     private String gender;
     private LocalDateTime timeCreated;
-    LocalDateTime timeEdited = LocalDateTime.of(2019, 1, 1, 0, 0);
+    LocalDateTime timeEdited = LocalDateTime.of(2019, 1, 1, 0,0);
 
     public Person(String firstName, String lastName, String birthDate, String gender, String phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthDate = birthDate;
-        if (birthDate.isBlank()) {
-            System.out.println("Bad birth date!");
-            this.birthDate = "[no data]";
-        } else this.birthDate = birthDate;
-        if (!gender.equals("M") && !gender.equals("M")) {
-            System.out.println("Bad gender!");
-            this.gender = "[no data]";
-        } else this.gender = gender;
+        this.gender = gender;
         this.phoneNumber = phoneNumber;
-        timeCreated = LocalDateTime.now();
+        this.timeCreated = LocalDateTime.now();
     }
+
+    @Override
+    String showName() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    void edit() {
+        System.out.print("Select a field (name, surname, birth date, gender, number): ");
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        switch (command) {
+            case "name":
+                System.out.print("Enter name: ");
+                this.firstName = sc.nextLine();
+                break;
+            case "surname":
+                System.out.print("Enter surname: ");
+                this.lastName = sc.nextLine();
+                break;
+            case "birth date":
+                this.birthDate = sc.nextLine();
+                break;
+            case "gender":
+                this.gender =sc.nextLine();
+                break;
+            case "number":
+                System.out.print("Enter number: ");
+                this.phoneNumber = Main.isNumberValid(sc.nextLine());
+                break;
+        }
+        timeEdited = LocalDateTime.now();
+        System.out.println("Saved");
+    }
+
 
     @Override
     public String toString() {
         return "Name: " + firstName + "\nSurname: " + lastName + "\nBirth date: "
                 + birthDate + "\nGender: " + gender + "\nNumber: " + phoneNumber
                 + "\nTime created: " + timeCreated + "\nTime last edit: " + timeEdited;
-    }
-
-    public String getName() {
-        return firstName + " " + lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
     }
 }
