9da36bfaaa804efe8dfa9f41cd13e131b69a483b
Tegridy <jarjak4@gmail.com>
2019-08-17 17:10:40 +0200

    Stage 3 final

--- Phone Book/task/src/phonebook/Main.java
+++ Phone Book/task/src/phonebook/Main.java
@@ -5,25 +5,61 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    private static List<String> directoryNames = new ArrayList<>();
+    static List<String> directoryNames = new ArrayList<>();
     private static List<String> findNames = new ArrayList<>();
 
     private static long totalTimeLinear;
     private static long totalTimeBubble;
+    private static long totalTimeBinary;
+
+    private static int counter;
 
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         processFiles();
-        System.out.println("Start searching (linear search)...");
-        System.out.println(linearSearch(false) + "\n");
-        System.out.println("Start searching (bubble sort + jump search)...");
-        bubbleSort();
+
+        System.out.println("Choose (0 - linear search, 1 - bubble sort + jump search, 2 - quick sort + binary search): ");
+        switch (sc.nextInt()){
+            case 0:
+                System.out.println("Start searching (linear search)...");
+                System.out.println(linearSearch(false));
+                break;
+            case 1:
+                System.out.println("Start searching (bubble sort + jump search)...");
+                bubbleSort();
+                break;
+            case 2:
+                System.out.println("Start searching (quick sort + binary search)...");
+
+                long startTime = System.nanoTime();
+                quickSort(0, directoryNames.size()-1);
+
+                long endTime = System.nanoTime();
+                long totalTime = endTime - startTime;
+
+                System.out.println("Sorting time: " + calculateTime(totalTime));
+                System.out.println(binarySearch());
+
+                long totalTimeQS = totalTime + totalTimeBinary;
+
+                System.out.println("Found " + counter + " / " + findNames.size() + " entries. " +
+                        "Time taken " + calculateTime(totalTimeQS));
+
+                break;
+            default:
+                System.out.println("Wrong choice.");
+                break;
+        }
+
+
     }
 
     private static void bubbleSort(){
@@ -65,7 +101,7 @@ public class Main {
 
         long startTime = System.nanoTime();
 
-        int counter = 0;
+        counter = 0;
 
         for (String name : findNames) {
             for (String person : directoryNames) {
@@ -94,15 +130,15 @@ public class Main {
         int prevRight = 0;
 
         long totalTimeJump = 0;
-        long totaltimelinear = 0;
+        long linearTime = 0;
 
         int jumpLength = (int) Math.sqrt(directoryNames.size());
 
-        int counter = 0;
+        counter = 0;
+
+        long startTime = System.nanoTime();
 
         while (counter != findNames.size()) {
-
-            long startTime = System.nanoTime();
 
             while (currentRight < directoryNames.size() - 1) {
 
@@ -122,10 +158,7 @@ public class Main {
 
             }
 
-
-
-            long endTime = 0;
-            endTime = System.nanoTime();
+            long endTime = System.nanoTime();
             totalTimeJump += endTime - startTime;
 
             // Linear
@@ -141,20 +174,86 @@ public class Main {
             }
 
             long endTimeLinear = System.nanoTime();
-            totaltimelinear += endTimeLinear - startLinear;
+            linearTime += endTimeLinear - startLinear;
 
         }
 
-        calculateTime(totalTimeJump);
-        calculateTime(totaltimelinear);
-
         System.out.print("Found " + counter + " / " + findNames.size());
-        System.out.println("Time taken " + calculateTime(totalTimeJump + totaltimelinear));
+        System.out.println("Time taken " + calculateTime(totalTimeJump + linearTime));
+    }
+
+    private static void quickSort(int left, int right){
+        if(left < right){
+            int pivotIndex = partition(left, right);
+            quickSort(left, pivotIndex-1);
+            quickSort(pivotIndex+1, right);
+        }
+    }
+
+    private static int partition(int left, int right){
+        String[] info = directoryNames.get(right).split(" ");
+        String pivot = info[1];
+        int partitionIndex = left;
+
+        for (int i = left; i < right; i++) {
+            String[] person = directoryNames.get(i).split(" ");
+            if (person[1].compareTo(pivot) < 0){
+                swap(i, partitionIndex);
+                partitionIndex++;
+            }
+        }
+
+        swap(partitionIndex, right);
+
+        return partitionIndex;
+    }
+
+    private static void swap(int i, int j){
+        String temp = directoryNames.get(i);
+        directoryNames.set(i, directoryNames.get(j));
+        directoryNames.set(j, temp);
+    }
+
+    private static String binarySearch() {
+
+        long startTime = System.nanoTime();
+
+        counter = 0;
+        for (String name : findNames) {
+
+            String[] info = name.split(" ");
+            String personToFind = info[0];
+
+            int left = 0;
+            int right = directoryNames.size() - 1;
+
+            while (left <= right) {
+
+                int mid = left + (right - left) / 2;
+
+                String[] info2 = directoryNames.get(mid).split(" ");
+                String personFromDictionary = info2[1];
+
+                if (personToFind.equals(personFromDictionary)) {
+                    counter++;
+                    break;
+                } else if (personToFind.compareTo(personFromDictionary) < 0) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+
+        long endTime = System.nanoTime();
+        totalTimeBinary = endTime - startTime;
+
+        return "Searching time: " + calculateTime(totalTimeBinary);
     }
 
     private static void processFiles(){
-        File directory = new File("C:\\Users\\Ja\\Desktop\\files\\directory.txt");
-        File find = new File("C:\\Users\\Ja\\Desktop\\files\\find.txt");
+        File directory = new File("C:\\IdeaProjects\\Phone Book\\Phone Book\\task\\src\\phonebook\\dictionary.txt");
+        File find = new File("C:\\IdeaProjects\\Phone Book\\Phone Book\\task\\src\\phonebook\\find.txt");
 
         try (Scanner sc = new Scanner(directory)){
             Scanner sc2 = new Scanner(find);
@@ -166,10 +265,12 @@ public class Main {
             while (sc2.hasNextLine()){
                 findNames.add(sc2.nextLine());
             }
+
+            System.out.println("File reading complete.");
+
         } catch (FileNotFoundException e){
             System.out.println("Problem with opening file.");
         }
-        System.out.println("File reading complete.");
     }
 
     private static String calculateTime(long totalTime){
@@ -177,10 +278,10 @@ public class Main {
         long minutes = TimeUnit.NANOSECONDS.toMinutes(totalTime);
         long seconds =  TimeUnit.NANOSECONDS.toSeconds(totalTime) -
                 TimeUnit.MINUTES.toSeconds(TimeUnit.NANOSECONDS.toMinutes(totalTime));
-        long miliSec = TimeUnit.NANOSECONDS.toMillis(totalTime) -
+        long milSec = TimeUnit.NANOSECONDS.toMillis(totalTime) -
                 TimeUnit.SECONDS.toMillis(TimeUnit.NANOSECONDS.toSeconds(totalTime));
 
-        return minutes + " min. " + seconds + " sec. " + miliSec + " ms.";
+        return minutes + " min. " + seconds + " sec. " + milSec + " ms.";
     }
 
 }
