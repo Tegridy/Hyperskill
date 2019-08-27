89e69cdfddd3f2eb393e36475355b141fcd874cc
Tegridy <jarjak4@gmail.com>
2019-08-27 15:12:16 +0200

    Stage 5

--- Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/task/src/sorting/Main.java
@@ -1,27 +1,34 @@
 package sorting;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        boolean sortingType = false; // false for natural, true = byCount
+
         if (args[0].equals("-dataType")){
-            switch (args[1].toLowerCase()){
+            if(args.length > 2 && (args[2].equals("-sortingType") && args[3].equals("byCount"))){
+                sortingType = true;
+            }
+            switch (args[1]){
                 case "long":
-                    readLong();
+                    readLong(sortingType);
                     break;
                 case "line":
-                    readLine();
+                    readLine(sortingType);
                     break;
                 case "word":
-                    readWord();
+                    readWord(sortingType);
                     break;
                 default:
-                    System.out.println("Wrong argument.");
+                    System.out.println("Wrong command!");
                     break;
             }
         }
@@ -29,8 +36,9 @@ public class Main {
 
     }
 
-    public static void readLong(){
+    public static void readLong(boolean sortingType){
         List<Long> numbers = new ArrayList<>();
+
 
         while (scanner.hasNextLong()) {
             try {
@@ -41,73 +49,112 @@ public class Main {
             }
         }
 
-        long max = 0;
-        long maxElements = 0;
-        int percentage = 0;
+        if (sortingType){
+            System.out.println("Total numbers: " + numbers.size());
 
-        System.out.println("Total numbers: " + numbers.size());
-        if (numbers.size() > 0) {
-            Predicate<Long> predicate = x -> x >= Collections.max(numbers);
-             max = numbers.stream().max(Long::compare).get();
-             maxElements = numbers.stream().filter(predicate).count();
-             percentage = (int) (maxElements*100) / numbers.size();
+            Map<Long, Long> dataEntryByCount = numbers.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            Map<Long, Long> result = new LinkedHashMap<>();
+            dataEntryByCount.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEachOrdered(e -> result.put(e.getKey(), e.getValue()));
+
+            for (Map.Entry e : result.entrySet()) {
+                System.out.println(e.getKey() + ": " + e.getValue() +" time(s), " + ((long)e.getValue() * 100) / numbers.size() + "%");
+            }
+
+        } else {
+
+            long max;
+            long maxElements;
+            int percentage;
+
+            System.out.println("Total numbers: " + numbers.size());
+            if (numbers.size() > 0) {
+                Predicate<Long> predicate = x -> x >= Collections.max(numbers);
+                max = numbers.stream().max(Long::compare).get();
+                maxElements = numbers.stream().filter(predicate).count();
+                percentage = (int) (maxElements * 100) / numbers.size();
+
+                System.out.println("The greatest number: " + max + " (" + maxElements + " time(s) " + percentage + "%).");
+            }
         }
-
-        System.out.println("The greatest number: " + max + " (" + maxElements + " time(s) " + percentage + "%).");
     }
 
-    public static void readLine(){
+    public static void readLine(boolean sortingType) {
         List<String> lines = new ArrayList<>();
 
         while (scanner.hasNextLine()) {
             try {
                 lines.add(scanner.nextLine());
-            } catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
                 break;
             }
         }
 
-        String max = "";
-        long maxElements = 0;
-        int percentage = 0;
+        if (sortingType) {
+            Map<String, Long> dataEntryByCount = lines.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            Map<String, Long> result = new LinkedHashMap<>();
+            dataEntryByCount.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEachOrdered(e -> result.put(e.getKey(), e.getValue()));
 
-        System.out.println("Total lines: " + lines.size());
-        if (lines.size() > 0) {
-            max = Collections.max(lines, Comparator.comparing(String::length));
-            String finalMax = max;
-            Predicate<String> predicate = x -> x.equals(finalMax);
-            maxElements = lines.stream().filter(predicate).count();
-            percentage = (int) (maxElements*100) / lines.size();
+            for (Map.Entry e : result.entrySet()) {
+                System.out.println(e.getKey() + ": " + e.getValue() + " time(s), " + ((long) e.getValue() * 100) / lines.size() + "%");
+            }
+        } else {
+
+            String max;
+            long maxElements;
+            int percentage;
+
+            System.out.println("Total lines: " + lines.size());
+            if (lines.size() > 0) {
+                max = Collections.max(lines, Comparator.comparing(String::length));
+                String finalMax = max;
+                Predicate<String> predicate = x -> x.equals(finalMax);
+                maxElements = lines.stream().filter(predicate).count();
+                percentage = (int) (maxElements * 100) / lines.size();
+
+                System.out.println("The longest line: \n" + max + "\n (" + maxElements + " time(s) " + percentage + "%).");
+            }
         }
-        System.out.println("The longest line: \n" + max + "\n (" + maxElements + " time(s) " + percentage + "%).");
     }
 
-    private static void readWord(){
+    private static void readWord(boolean sortingType) {
         List<String> words = new ArrayList<>();
 
         while (scanner.hasNext()) {
             try {
                 words.add(scanner.next());
-            } catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
                 break;
             }
         }
 
-        String max = "";
-        long maxElements = 0;
-        int percentage = 0;
+        String max;
+        long maxElements;
+        int percentage;
 
         System.out.println("Total words: " + words.size());
-        if (words.size() > 0) {
-            max = Collections.max(words, Comparator.comparing(String::length));
-            String finalMax = max;
-            Predicate<String> predicate = x -> x.equals(finalMax);
-            maxElements = words.stream().filter(predicate).count();
-            percentage = (int) (maxElements*100) / words.size();
+
+        if (sortingType) {
+            Map<String, Long> dataEntryByCount = words.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            Map<String, Long> result = new LinkedHashMap<>();
+            dataEntryByCount.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEachOrdered(e -> result.put(e.getKey(), e.getValue()));
+
+            for (Map.Entry e : result.entrySet()) {
+                System.out.println(e.getKey() + ": " + e.getValue() + " time(s), " + ((long) e.getValue() * 100) / words.size() + "%");
+            }
+        } else {
+
+            if (words.size() > 0) {
+                max = Collections.max(words, Comparator.comparing(String::length));
+                String finalMax = max;
+                Predicate<String> predicate = x -> x.equals(finalMax);
+                maxElements = words.stream().filter(predicate).count();
+                percentage = (int) (maxElements * 100) / words.size();
+
+                System.out.println("The longest word: " + max + " (" + maxElements + " time(s) " + percentage + "%).");
+            }
         }
-        System.out.println("The longest word: " + max + " (" + maxElements + " time(s) " + percentage + "%).");
     }
 
 }
