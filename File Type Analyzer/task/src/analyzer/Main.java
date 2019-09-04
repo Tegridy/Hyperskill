8daa8c2de8943ec13cf49273cb7f2a59b9dc1d48
Tegridy <jarjak4@gmail.com>
2019-09-04 12:55:08 +0200

    Stage 2

--- File Type Analyzer/task/src/analyzer/Main.java
+++ File Type Analyzer/task/src/analyzer/Main.java
@@ -1,39 +1,42 @@
 package analyzer;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Map<byte[], String> extensionsMap = new HashMap<>();
 
-        byte[] pdfSignature = args[1].getBytes();
+        byte[] signature = args[2].getBytes();
 
-        extensionsMap.put(pdfSignature, args[2]);
-
+        long startTime, endTime, totalTime;
 
         try {
-            byte[] allBytes = Files.readAllBytes(Path.of(args[0]));
+            byte[] allBytes = Files.readAllBytes(Path.of(args[1]));
 
-            System.out.println(find(allBytes, pdfSignature) == 1 ? args[2] : "Unknown file type");
-
+            if (args[0].equals("--naive")){
+                startTime = System.nanoTime();
+                System.out.println(naive(allBytes, signature) == 1 ? args[3] : "Unknown file type");
+                endTime = System.nanoTime();
+                totalTime = endTime - startTime;
+                System.out.println("It took: " + (double) TimeUnit.NANOSECONDS.toSeconds(totalTime) + " seconds");
+            } else if (args[0].equals("--KMP")){
+                startTime = System.nanoTime();
+                System.out.println(indexOf(allBytes, signature) == 1 ? args[3] : "Unknown file type");
+                endTime = System.nanoTime();
+                totalTime = endTime - startTime;
+                System.out.println("It took: " + (double) TimeUnit.NANOSECONDS.toSeconds(totalTime) + " seconds");
+            }
         } catch (Exception e){
             e.printStackTrace();
         }
 
     }
 
-    static int find(byte[] allBytes, byte[] fileType){
+    static int naive(byte[] allBytes, byte[] fileType){
 
         for(int i = 0; i < allBytes.length - fileType.length+1; ++i) {
             boolean found = true;
@@ -49,40 +52,41 @@ public class Main {
         return -1;
     }
 
-//    private static int indexOf(byte[] data, byte[] pattern) {
-//        if (data.length == 0) return -1;
-//
-//        int[] failure = computeFailure(pattern);
-//        int j = 0;
-//
-//        for (int i = 0; i < data.length; i++) {
-//            while (j > 0 && pattern[j] != data[i]) {
-//                j = failure[j - 1];
-//            }
-//            if (pattern[j] == data[i]) { j++; }
-//            if (j == pattern.length) {
-//                return i - pattern.length + 1;
-//            }
-//        }
-//        return -1;
-//    }
-//
-//    private static int[] computeFailure(byte[] pattern) {
-//        int[] failure = new int[pattern.length];
-//
-//        int j = 0;
-//        for (int i = 1; i < pattern.length; i++) {
-//            while (j > 0 && pattern[j] != pattern[i]) {
-//                j = failure[j - 1];
-//            }
-//            if (pattern[j] == pattern[i]) {
-//                j++;
-//            }
-//            failure[i] = j;
-//        }
-//
-//        return failure;
-//    }
+
+    private static int indexOf(byte[] data, byte[] pattern) {
+        if (data.length == 0) return -1;
+
+        int[] failure = KMPsearch(pattern);
+        int j = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            while (j > 0 && pattern[j] != data[i]) {
+                j = failure[j - 1];
+            }
+            if (pattern[j] == data[i]) { j++; }
+            if (j == pattern.length) {
+                return i - pattern.length + 1;
+            }
+        }
+        return -1;
+    }
+
+    private static int[] KMPsearch(byte[] pattern) {
+        int[] failure = new int[pattern.length];
+
+        int j = 0;
+        for (int i = 1; i < pattern.length; i++) {
+            while (j > 0 && pattern[j] != pattern[i]) {
+                j = failure[j - 1];
+            }
+            if (pattern[j] == pattern[i]) {
+                j++;
+            }
+            failure[i] = j;
+        }
+
+        return failure;
+    }
 
 
 }
