c62a136209a39beef566556d50726f65ac491786
Tegridy <jarjak4@gmail.com>
2019-10-01 09:32:09 +0200

    Stage 1

---
+++ Minesweeper/task/src/minesweeper/Main.java
@@ -0,0 +1,16 @@
+package minesweeper;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(".X.......\n" +
+                ".....X..X\n" +
+                "....X....\n" +
+                "......X..\n" +
+                "..X......\n" +
+                "....X....\n" +
+                "..X......\n" +
+                "..X......\n" +
+                "......X..");
+    }
+}
