53fc250561fd1399b36317205b074cb5afd2c34d
Tegridy <jarjak4@gmail.com>
2019-10-20 17:27:36 +0200

    Stage 3

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -4,37 +4,77 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner sc = new Scanner(System.in);
+
+    private static String[] data;
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        readData();
+        printMenu();
+    }
+
+    private static void searchData() {
+            System.out.println("Enter a name or email to search all suitable people: ");
+            String name = sc.nextLine().toLowerCase();
+
+            System.out.println("Found people: ");
+            for (String info : data) {
+                if (info.toLowerCase().contains(name)) {
+                    System.out.println(info);
+                }
+            }
+    }
+
+    private static void readData(){
         System.out.println("Enter the number of people: ");
         int numOfData = sc.nextInt();
         sc.nextLine();
 
-        String[] data = new String[numOfData];
+        data = new String[numOfData];
 
         System.out.println("Enter all people: ");
         for (int i = 0; i < data.length; i++) {
             data[i] = sc.nextLine();
         }
         System.out.println();
-
-        System.out.println("Enter the number of search queries: ");
-        int numOfQueries = sc.nextInt();
-        sc.nextLine();
-        System.out.println();
-
-        for (int i = 0; i < numOfQueries; i++) {
-            System.out.println("Enter data to search people: ");
-            String name = sc.nextLine().trim();
-
-            System.out.println("Found people: ");
-            for (String info : data) {
-                if (info.contains(name)) {
-                    System.out.println(info);
-                }
-            }
-        }
-
     }
 
+    private static void printMenu(){
+        boolean quit = false;
+        while (!quit) {
+
+            System.out.println(" === Menu === ");
+            System.out.println("1. Find a person");
+            System.out.println("2. Print all people");
+            System.out.println("0. Exit");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 1:
+                    searchData();
+                    break;
+                case 2:
+                    printAll();
+                    break;
+                case 0:
+                    System.out.println("Bye!");
+                    quit = true;
+                    break;
+                default:
+                    System.out.println("Incorrect option! Try again.");
+                    break;
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printAll(){
+        System.out.println("=== List of people ===");
+        for(String e : data){
+            System.out.println(e);
+        }
+
+        System.out.println();
+    }
 }
